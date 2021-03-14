1693baf3ae1030543e78df2c8c95e7e1866935a5
eddie9712 <qw1562435@gmail.com>
2021-03-14 14:28:05 +0800

    Control LEDs on STM32
    Start with the first state and push the blue button for the second
    state

---
+++ Lab01/Debug/FreeRTOS/event_groups.d
@@ -0,0 +1,40 @@
+FreeRTOS/event_groups.o: ../FreeRTOS/event_groups.c \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/FreeRTOS.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/FreeRTOSConfig.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/projdefs.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/portable.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/deprecated_definitions.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/mpu_wrappers.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/task.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/list.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/timers.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/task.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/event_groups.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/timers.h
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/FreeRTOS.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/FreeRTOSConfig.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/projdefs.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/portable.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/deprecated_definitions.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/mpu_wrappers.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/task.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/list.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/timers.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/task.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/event_groups.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/timers.h:
---
+++ Lab01/Debug/FreeRTOS/queue.d
@@ -0,0 +1,34 @@
+FreeRTOS/queue.o: ../FreeRTOS/queue.c \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/FreeRTOS.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/FreeRTOSConfig.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/projdefs.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/portable.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/deprecated_definitions.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/mpu_wrappers.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/task.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/list.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/queue.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/task.h
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/FreeRTOS.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/FreeRTOSConfig.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/projdefs.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/portable.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/deprecated_definitions.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/mpu_wrappers.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/task.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/list.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/queue.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab1_P76091331/FreeRTOS/include/task.h:
