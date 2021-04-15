f978794652279b78de1b9fb1db69eec8e70940b9
eddie9712 <qw1562435@gmail.com>
2021-04-15 08:51:38 +0800

    Add project "lab2" to the repository

---
+++ Lab02/Debug/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,28 @@
+FreeRTOS/portable/MemMang/heap_4.o: ../FreeRTOS/portable/MemMang/heap_4.c \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/FreeRTOS.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/FreeRTOSConfig.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/projdefs.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/portable.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/deprecated_definitions.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/mpu_wrappers.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/task.h \
+ /home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/list.h
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/FreeRTOS.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/FreeRTOSConfig.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/projdefs.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/portable.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/deprecated_definitions.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/mpu_wrappers.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/task.h:
+
+/home/eddie/STM32CubeIDE/workspace_1.5.1/Embedded_OS_Lab2_P76091331/FreeRTOS/include/list.h:
